c87c70d8f6769665c79618248bc831128a2dd065
SanjanaReddy2005 <saisanjanareddyalgubelly@gmail.com>
2023-10-31 16:26:09 +0530

    tables

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -86,7 +86,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':'browsebytes',
         'USER':'root',
-        'PASSWORD':'root',
+        'PASSWORD':'Harsan@2005',
         'PORT':3306,
         'HOST':'127.0.0.1'
     }
@@ -144,12 +144,12 @@ REST_FRAMEWORK = {
     ),
 }
 
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
-EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
-EMAIL_USE_TLS = True
+# EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+# EMAIL_HOST = "smtp.gmail.com"
+# EMAIL_PORT = 587
+# EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+# EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
+# EMAIL_USE_TLS = True
 
 
 SIMPLE_JWT = {
