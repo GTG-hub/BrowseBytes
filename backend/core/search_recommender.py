ab4fd3a845f5e5a748e6e0561d770881cc22c392
Mishajain1110 <117731761+Mishajain1110@users.noreply.github.com>
2023-11-02 02:18:34 +0530

    Recommender_article model

--- backend/core/search_recommender.py
+++ backend/core/search_recommender.py
@@ -1,10 +1,17 @@
+# pip install -U sentence-transformers
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 
 model = SentenceTransformer("sentence-transformers/all-mpnet-base-v2")
 
-sentences = ["I am idiot", "Each sentence is converted"]
-query = "This is converted"
+# titles of all 40 articles
+sentences = ["I am idiot", "Each sentence is converted", "Hello this", "I have converted misha"]
+list_articles = ["Idiot", "I", "Misha", "Converted"]
+
+query = ""
+for title in list_articles:
+    query += title
+
 
 def recommender_search(sentences: list, query: str):
     final_list = []
@@ -23,7 +30,7 @@ def recommender_search(sentences: list, query: str):
     for element in my_list:
         final_list.append(sentences[dict[element]])
 
-    return final_list
+    return final_list[: 3]
 
 
 print(recommender_search(sentences, query))
--- backend/core/summarizer.py
+++ backend/core/summarizer.py
@@ -3,6 +3,8 @@ import torch
 from transformers import pipeline
 
 # article summarizer model
-def article_summarizer(text : str):
+
+
+def article_summarizer(text: str):
     summarizer = pipeline("summarization", model="facebook/bart-large-cnn")
     return summarizer(text, max_length=1000, min_length=130, do_sample=False)
