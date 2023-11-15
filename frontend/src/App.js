74c8e03c745f936a6b72d33b5d216548e42c77d4
Mishajain1110 <117731761+Mishajain1110@users.noreply.github.com>
2023-11-16 01:38:29 +0530

    summary page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { Home } from "./pages/Home";
 import { SearchResults } from "./pages/Searchresults";
 import { Search } from "./pages/Search";
 import { Signin_Register } from "./pages/Signin_Register";
+import { Summary } from "./pages/Summary";
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
           <Route path="/Profile/" element={<Profile />} />
           <Route path="/Search_Results/" element={<SearchResults />} />
           <Route path="/Signin_Register" element={<Signin_Register />} />
+          <Route path="/Summary" element={<Summary />} />
           <Route path="/Search" element={<Search />} />
         </Routes>
       </Router>
---
+++ frontend/src/pages/Summary.jsx
@@ -0,0 +1,79 @@
+import React, { useEffect } from "react";
+import { Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./summary_style.css";
+
+function Summary() {
+  return (
+    <div className="myclass">
+      <nav class="navbar navbar-light" style="margin-bottom: 5px">
+        <form class="form-inline ml-auto" style="max-width: 500px">
+          <input
+            class="form-control mr-sm-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+            style="width: 70%"
+          />
+          <button class="btn btn-primary my-2 my-sm-0" type="submit">
+            Search
+          </button>
+        </form>
+      </nav>
+      <h1>SUMMARY</h1>
+      <div class="container">
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae
+          delectus vel quibusdam magni, corporis animi labore tenetur, inventore
+          enim facilis eaque! Atque quibusdam est minima quod totam nobis.
+          Debitis laudantium suscipit quos ad, labore voluptas, eius quia
+          quibusdam repellat sapiente expedita repellendus dolore laboriosam,
+          sequi perferendis. Soluta temporibus labore ad optio aliquid fugiat
+          veritatis distinctio. Distinctio quidem dolores rerum, repellat, quasi
+          molestiae nemo veniam totam a saepe ratione magni, animi iste earum
+          possimus reprehenderit aperiam. Velit harum magnam debitis soluta
+          beatae, distinctio, nulla asperiores consequuntur at laudantium
+          repudiandae, facilis nobis unde rerum accusantium! Quam obcaecati
+          mollitia odit quidem fugiat, error aut maiores non ex veritatis
+          dolorem autem rerum numquam laborum, quibusdam voluptatibus pariatur
+          voluptates accusantium voluptate cum laudantium aperiam perferendis
+          quo? Quibusdam voluptatem eveniet veritatis sunt dolore quasi in
+          commodi atque delectus, magnam ipsum veniam, consectetur illo animi
+          nulla. Soluta dignissimos eligendi minus quo quia dolores pariatur,
+          qui ducimus nulla quis in tenetur numquam tempora quae officia modi
+          voluptatem incidunt, rem maiores temporibus molestiae fuga non error
+          amet. Eos in labore eligendi quae quidem aspernatur quas vitae dolores
+          rerum? Nobis delectus debitis quas sequi beatae officia tempore
+          placeat doloremque voluptas blanditiis eum dolorem, consectetur fugit
+          minus dolore magnam saepe maxime voluptatem ipsam quisquam. Explicabo
+          commodi magni ad voluptatibus dolorem atque amet eum voluptatem!
+          Recusandae enim deserunt totam modi natus vitae molestias vel ullam
+          delectus quibusdam? Maiores repudiandae autem, voluptates possimus
+          sequi, rem dicta quo blanditiis vero velit iure fuga harum amet quia
+          adipisci asperiores! Et nostrum ad debitis cupiditate quaerat quas
+          dolorem laborum maiores ducimus adipisci reprehenderit magnam repellat
+          veritatis, vitae repellendus! Neque voluptates perferendis cupiditate
+          illum itaque incidunt quia eaque corrupti, maiores nihil, at, beatae
+          inventore. Laboriosam voluptate quisquam distinctio molestias, unde
+          sequi velit atque. Dicta at eaque accusamus odio a omnis amet
+          similique natus, ab perspiciatis dolorem quas deleniti quos tempora
+          temporibus molestiae fugiat rerum. Maxime voluptates sunt voluptatum
+          quo minima saepe ab autem laudantium placeat itaque qui, ad ipsum
+          tenetur temporibus sequi. Quibusdam ab ad libero at, sint ut
+          laudantium enim consectetur qui nulla modi molestias praesentium saepe
+          aliquid illo et autem animi. At harum pariatur atque ipsa explicabo
+          hic sed, dolores enim! Reprehenderit eligendi ea cumque, vitae natus
+          officiis. Hic, ab molestias. Similique mollitia odit, a debitis porro
+          assumenda accusamus consequatur explicabo officia ullam atque nihil
+          unde consectetur vero! Impedit maiores corrupti asperiores excepturi,
+          ullam vero ratione, necessitatibus ab placeat consequatur velit
+          veritatis. Aperiam quis sit quia ad inventore earum, nemo, laboriosam
+          cupiditate ab exercitationem ea voluptas. Accusantium quasi amet
+          excepturi at laborum, voluptates eligendi voluptate error ipsum magnam
+          eaque consequuntur.
+        </p>
+      </div>
+    </div>
+  );
+}
+export default Summary;
