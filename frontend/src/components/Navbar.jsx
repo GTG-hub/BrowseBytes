0f64c8a808b81a7444a8d5e5a91f1a2f869f018e
Aditi Wekhande <aditi.wekhande@gmail.com>
2023-11-17 03:28:13 +0530

    navbar changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -3,15 +3,16 @@ import '../../node_modules/bootstrap/dist/css/bootstrap.css';
 
 export const Navbar = () => {
     return (
-        <nav class="navbar bg-body-tertiar fixed ">
+        <nav class="navbar bg-body-tertiar fixed" style={{background: '#011f4b'}}>
   <div class="container-fluid">
-    <a class="navbar-brand" href="/">Offcanvas navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <a class="navbar-brand" style={{color: 'white'}} href="/">Home</a>
+    <button class="navbar-toggler" style={{color: 'white', background: 'white'}} type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation">
+    {/* <button class="navbar-toggler" style={{color: 'white', background: 'white'}} type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar" aria-label="Toggle navigation"> */}
+      <span style={{color: 'white'}} class="navbar-toggler-icon"></span>
     </button>
     <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
       <div class="offcanvas-header">
-        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">Offcanvas</h5>
+        <h5 class="offcanvas-title" id="offcanvasNavbarLabel">BrowseBytes</h5>
         <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
       </div>
       <div class="offcanvas-body">
@@ -20,9 +21,12 @@ export const Navbar = () => {
             <a class="nav-link active" aria-current="page" href="/">Home</a>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="/">Link</a>
+            <a class="nav-link" href="/profile">Profile</a>
           </li>
-          <li class="nav-item dropdown">
+          <li class="nav-item">
+            <a class="nav-link" href="/signin">Sign In</a>
+          </li>
+          {/* <li class="nav-item dropdown">
             <a class="nav-link dropdown-toggle" href="/" role="button" data-bs-toggle="dropdown" aria-expanded="false">
               Dropdown
             </a>
@@ -34,11 +38,11 @@ export const Navbar = () => {
               </li>
               <li><a class="dropdown-item" href="/">Something else here</a></li>
             </ul>
-          </li>
+          </li> */}
         </ul>
         <form class="d-flex mt-3" role="search">
           <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button class="btn btn-outline-success" type="submit">Search</button>
+          <button class="btn" style={{background: '#011f4b', color:'white'}} type="submit">Search</button>
         </form>
       </div>
     </div>
